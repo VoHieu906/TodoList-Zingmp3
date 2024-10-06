e92b62894c677599c71fb9cc8f690b445e909807
VoHieu906 <vohieutd09@gmail.com>
2024-10-06 19:12:48 +0700

    add chart, list for zingchart page

--- src/containers/public/ZingChart.jsx
+++ src/containers/public/ZingChart.jsx
@@ -1,8 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { apiGetChartHome } from "../../apis";
+import { Line } from "react-chartjs-2";
+import { Chart, scales } from "chart.js/auto";
+import { SongItem, ListItem } from "../../components";
+import _ from "lodash";
 const ZingChart = () => {
   const [chartData, setChartData] = useState();
   const [data, setData] = useState(null);
+  const chartRef = useRef();
+  const [selected, setSelected] = useState(false);
+  const [tooltipState, setTooltipState] = useState({
+    opcaity: 0,
+    top: 0,
+    left: 0,
+  });
+  const options = {
+    responsive: true,
+    pointRadius: 0,
+    maintainAspecRatio: false,
+    scales: {
+      y: {
+        ticks: { display: false },
+        grid: { color: "rgba(0,0,0,0.3)", drawTicks: false },
+        min: chartData?.RTChart?.chart?.minScore,
+        max: chartData?.RTChart?.chart?.maxScore,
+        border: { dash: [3, 4] },
+      },
+      x: {
+        ticks: { color: "gray" },
+        grid: { color: "transparent" },
+      },
+    },
+    plugins: {
+      legend: false,
+      tooltip: {
+        enabled: false,
+        external: ({ tooltip }) => {
+          if (!chartRef || !chartRef.current) return;
+          if (tooltip.opacity === 0) {
+            if (tooltipState.opacity !== 0)
+              setTooltipState((prev) => ({ ...prev, opacity: 0 }));
+            return;
+          }
+          const counters = [];
+          for (let i = 0; i < 3; i++) {
+            counters.push({
+              data: chartData?.RTChart?.chart?.items[
+                Object.keys(chartData?.RTChart?.chart?.items)[i]
+              ]
+                ?.filter((item) => +item.hour % 2 === 0)
+                ?.map((item) => item.counter),
+              encodeId: Object.keys(chartData?.RTChart?.chart?.items)[i],
+            });
+          }
+          const rs = counters.find((i) =>
+            i.data.some(
+              (n) => n === +tooltip.body[0]?.lines[0]?.replace(",", "")
+            )
+          );
+          setSelected(rs.encodeId);
+
+          const newTooltipData = {
+            opacity: 1,
+            top: tooltip.caretY,
+            left: tooltip.caretX,
+          };
+          if (!_.isEqual(tooltipState, newTooltipData))
+            setTooltipState(newTooltipData);
+        },
+      },
+    },
+    hover: {
+      mode: "dataset",
+      intersect: false,
+    },
+  };
   useEffect(() => {
     const fetchChartData = async () => {
       const response = await apiGetChartHome();
@@ -43,9 +115,58 @@ const ZingChart = () => {
   return (
     <div className="px-3 d-flex flex-column">
       <div className="d-flex align-items-center">
-        <h3>#zingchart</h3> <span>icon</span>
+        <h3 style={{ fontWeight: "bold", fontSize: 40, color: "#0E8080" }}>
+          #zingchart
+        </h3>
+        <span>icon</span>
       </div>
-      <div>chart</div>
+      <div className="mt-5">
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            height: 350,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {data && <Line data={data} ref={chartRef} options={options} />}
+          <div
+            className="tooltip"
+            style={{
+              top: tooltipState.top,
+              left: tooltipState.left,
+              opacity: tooltipState.opacity,
+              position: "absolute",
+            }}
+          >
+            <SongItem
+              thumbnail={
+                chartData?.RTChart?.items.find((i) => i.encodeId === selected)
+                  ?.thumbnail
+              }
+              title={
+                chartData?.RTChart?.items.find((i) => i.encodeId === selected)
+                  ?.title
+              }
+              artists={
+                chartData?.RTChart?.items.find((i) => i.encodeId === selected)
+                  ?.artistsNames
+              }
+              sid={
+                chartData?.RTChart?.items.find((i) => i.encodeId === selected)
+                  ?.encodeId
+              }
+              style={"bg-light"}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-5">
+        {chartData?.RTChart?.items.map((item) => (
+          <ListItem key={item.encodeId} songData={item} />
+        ))}
+      </div>
     </div>
   );
 };
